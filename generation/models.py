ef0dc6f22f11591f4376337e4a8e88d838731d96
BBolatA <neuralnetnimble@gmail.com>
2025-04-26 18:53:48 +0500

    add default values

--- generation/models.py
+++ generation/models.py
@@ -15,9 +15,9 @@ class GenerationTask(models.Model):
     qty = models.PositiveSmallIntegerField(default=1)
     style_selections = models.JSONField(default=list, blank=True)
     base_model_name = models.CharField(max_length=200, blank=True)
-    performance_selection = models.CharField(max_length=50, null=True, blank=True)
-    aspect_ratios_selection = models.CharField(max_length=20, null=True, blank=True)
-    save_extension = models.CharField(max_length=10, null=True, blank=True)
+    performance_selection = models.CharField(max_length=50, default="Speed", blank=True)
+    aspect_ratios_selection = models.CharField(max_length=20, default="1152*896", blank=True)
+    save_extension = models.CharField(max_length=10, default="png", blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
 
 
